437d22af18a69c3a0d74167718131d87047c3bb3
kckst8 <kckst8@gmail.com>
2017-11-04 12:02:23 -0400

    use ViewChild, Renderer service, and ElementRef

--- src/app/heroes/hero/add-hero/add-hero.component.ts
+++ src/app/heroes/hero/add-hero/add-hero.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, AfterViewInit } from '@angular/core';
+import { Hero } from 'app/heroes/hero/hero';
 
 @Component({
-  selector: 'app-add-hero',
+  selector: 'add-hero',
   templateUrl: './add-hero.component.html',
   styleUrls: ['./add-hero.component.css']
 })
 export class AddHeroComponent implements OnInit {
+  public hero: Hero;
+  private wrapperElement; 
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit() {
+    this.hero = new Hero();
+    console.log(this.el);
   }
 
+  ngAfterViewInit() {
+    for (let i = 0; i < this.el.nativeElement.children.length; i++) {
+      if (this.el.nativeElement.children[i].id === 'wrapper') {
+        this.wrapperElement = this.el.nativeElement.children[i];
+      }
+    }
+  }
+
+  public open() {
+    if (this.wrapperElement) {
+      this.renderer.setStyle(this.wrapperElement, 'display', 'block');
+    }
+  }
+
+  public close() {
+    if (this.wrapperElement) {
+      this.renderer.setStyle(this.wrapperElement, 'display', 'none');
+    }
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HeroesService } from './heroes.service';
 import { Hero } from './hero/hero';
+import { AddHeroComponent} from './hero/add-hero/add-hero.component';
 
 @Component({
     selector: 'heroes',
     templateUrl: './heroes.component.html',
     styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit, AfterViewInit {
     public heroes: Array<Hero>;
     public selectedHero: string;
+
+    @ViewChild(AddHeroComponent) addHeroComponent: AddHeroComponent;
 
     constructor(
         private heroesService : HeroesService
@@ -18,14 +21,21 @@ export class HeroesComponent implements OnInit {
     }
 
     ngOnInit() {
-       // this.heroes = [];
         this.heroesService.getHeroes().subscribe(data => {
             this.heroes = data.heroes;
         });
     }
 
+    ngAfterViewInit() {
+        console.log(this.addHeroComponent);
+    }
+
     public selectHero(hero) {
         this.selectedHero = hero;
         console.log(this.selectedHero);
+    }
+
+    public addHero() {
+        this.addHeroComponent.open();
     }
 }
